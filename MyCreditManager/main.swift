df6b142997f1b1333402c73c408123fcea0e7a7b
sohee120 <yuhsohee120@gmail.com>
2023-04-26 19:37:02 +0900

    변수 선언

---
+++ MyCreditManager/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  MyCreditManager
+//
+//  Created by 윤소희 on 2023/04/26.
+//
+
+import Foundation
+
+var continueProgram: Bool = true
+
+struct Student {
+    var name: String
+    var subject: [String : String]?
+}
+
+var students = [Student]()
+
+
