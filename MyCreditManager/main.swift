e639beb248fd1c723095973d469e05d64cc08518
sohee120 <yuhsohee120@gmail.com>
2023-04-26 20:00:29 +0900

    메뉴에 따른 스위치문 작성

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -16,4 +16,52 @@ struct Student {
 
 var students = [Student]()
 
+func getGrade(grade: String?) -> Double {
+    return 0
+}
+
+func addStudent() {
+    
+}
+
+func removeStudent() {
+    
+}
+
+func updateGrade() {
+    
+}
+
+func removeGrade() {
+    
+}
+
+func showAverage() {
+    
+}
+
+while continueProgram {
+    print("원하는 기능을 입력해주세요")
+    print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X:종료")
+    
+    let input = readLine()!
+    
+    switch input {
+    case "1":
+        addStudent()
+    case "2":
+        removeStudent()
+    case "3":
+        updateGrade()
+    case "4":
+        removeGrade()
+    case "5":
+        showAverage()
+    case "X":
+        continueProgram = false
+    default:
+        break
+    }
+    
+}
 
